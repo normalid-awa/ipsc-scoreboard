fa34994ad9b2e4824cd5149a4d4ab3d312466162
jkh675 <rluo2018@gmail.com>
2025-05-31 15:33:55 +0800

    Add login route

--- apps/web/src/components/navigation/RouteList.tsx
+++ apps/web/src/components/navigation/RouteList.tsx
@@ -1,12 +1,17 @@
 import { Route } from "@/app/routeList";
 import { Route as NextRoute } from "next";
 import {
+	Divider,
 	List,
 	ListItem,
 	ListItemButton,
 	ListItemIcon,
 	ListItemText,
+	Stack,
 } from "@mui/material";
+import { Login } from "@mui/icons-material";
+import { ReactNode } from "react";
+import { useConvexAuth } from "convex/react";
 
 export interface RouteListProps {
 	routes: Route[];
@@ -14,68 +19,104 @@ export interface RouteListProps {
 	open: boolean;
 }
 
-export default function RouteList(props: RouteListProps) {
+function RouteItem(props: {
+	name: string;
+	icon: ReactNode;
+	path: NextRoute;
+	navTo: (path: NextRoute) => () => void;
+	open: boolean;
+}) {
 	return (
-		<List>
-			{props.routes.map((route) => {
-				if (route.shouldBeShow && !route.shouldBeShow()) {
-					return null;
-				}
-				return (
-					<ListItem
-						key={route.path}
-						disablePadding
-						sx={{ display: "block" }}
-					>
-						<ListItemButton
-							onClick={props.navTo(route.path)}
-							sx={[
-								{
-									minHeight: 48,
-									px: 2.5,
+		<ListItem key={props.path} disablePadding>
+			<ListItemButton
+				onClick={props.navTo(props.path)}
+				sx={[
+					{
+						minHeight: 48,
+						px: 2.5,
+					},
+					props.open
+						? {
+								justifyContent: "initial",
+							}
+						: {
+								justifyContent: "center",
+							},
+				]}
+			>
+				<ListItemIcon
+					sx={[
+						{
+							minWidth: 0,
+							justifyContent: "center",
+						},
+						props.open
+							? {
+									mr: 3,
+								}
+							: {
+									mr: "auto",
 								},
-								props.open
-									? {
-											justifyContent: "initial",
-										}
-									: {
-											justifyContent: "center",
-										},
-							]}
-						>
-							<ListItemIcon
-								sx={[
-									{
-										minWidth: 0,
-										justifyContent: "center",
-									},
-									props.open
-										? {
-												mr: 3,
-											}
-										: {
-												mr: "auto",
-											},
-								]}
-							>
-								{route.icon}
-							</ListItemIcon>
-							<ListItemText
-								primary={route.name}
-								sx={[
-									props.open
-										? {
-												opacity: 1,
-											}
-										: {
-												opacity: 0,
-											},
-								]}
-							/>
-						</ListItemButton>
-					</ListItem>
-				);
-			})}
-		</List>
+					]}
+				>
+					{props.icon}
+				</ListItemIcon>
+				<ListItemText
+					primary={props.name}
+					sx={[
+						props.open
+							? {
+									opacity: 1,
+								}
+							: {
+									opacity: 0,
+								},
+					]}
+				/>
+			</ListItemButton>
+		</ListItem>
+	);
+}
+
+export default function RouteList(props: RouteListProps) {
+	const { isAuthenticated } = useConvexAuth();
+
+	return (
+		<Stack
+			justifyContent={"space-between"}
+			sx={{ height: "100%" }}
+			divider={<Divider />}
+		>
+			<List>
+				{props.routes.map((route) => {
+					if (route.shouldBeShow && !route.shouldBeShow()) {
+						return null;
+					}
+					return (
+						<RouteItem
+							key={route.path}
+							name={route.name}
+							icon={route.icon}
+							path={route.path}
+							navTo={props.navTo}
+							open={props.open}
+						/>
+					);
+				})}
+			</List>
+			{isAuthenticated ? (
+				<></>
+			) : (
+				<List>
+					<RouteItem
+						icon={<Login />}
+						name="Sign In"
+						path="/signin"
+						navTo={props.navTo}
+						open={props.open}
+					/>
+				</List>
+			)}
+		</Stack>
 	);
 }
