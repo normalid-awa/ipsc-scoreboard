0def5edc78d938e0872e500e6e7adce68bd8a64f
jkh675 <rluo2018@gmail.com>
2025-06-07 19:36:28 +0800

    Add getAuthUserInfo route

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -17,6 +17,7 @@ import type * as auth from "../auth.js";
 import type * as healthCheck from "../healthCheck.js";
 import type * as http from "../http.js";
 import type * as todos from "../todos.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -31,6 +32,7 @@ declare const fullApi: ApiFromModules<{
   healthCheck: typeof healthCheck;
   http: typeof http;
   todos: typeof todos;
+  users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,25 @@
+import { ConvexError } from "convex/values";
+import { query } from "./_generated/server";
+
+export const getAuthUserIdentity = query({
+	handler: async (ctx): Promise<{ avatar: string; name: string }> => {
+		const id = await ctx.auth.getUserIdentity();
+		if (!id)
+			return {
+				avatar: "",
+				name: "Guest",
+			};
+
+		const data = (
+			await ctx.db
+				.query("users")
+				.filter((q) => q.eq(q.field("_id"), id.subject.split("|")[0]))
+				.take(1)
+		)[0];
+
+		return {
+			avatar: data.image || "",
+			name: data.name || "Unknown",
+		};
+	},
+});
