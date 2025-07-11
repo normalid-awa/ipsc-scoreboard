787ead2ddb4a9f41de0a03a9a14aa4175f6dbe44
jkh675 <rluo2018@gmail.com>
2025-07-11 10:04:57 +0800

    fix style

--- apps/web/src/app/timer/page.tsx
+++ apps/web/src/app/timer/page.tsx
@@ -3,10 +3,8 @@ import Timer from "@/components/timer/Timer";
 
 export default function TimerPage() {
 	return (
-		<div>
-			<Paper elevation={2} sx={{ p: 2 }}>
-				<Timer />
-			</Paper>
-		</div>
+		<Paper elevation={2} sx={{ p: 2, height: "100%" }}>
+			<Timer />
+		</Paper>
 	);
 }
