99887a4b3c17394cbd577b61482ccc468eba6333
jkh675 <rluo2018@gmail.com>
2025-09-01 22:43:29 +0800

    feat(api): Mikro-orm integration

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,14 @@
 import { Elysia } from "elysia";
 import { node } from "@elysiajs/node";
+import orm from "./database/orm.js";
+import { RequestContext, Utils, wrap } from "@mikro-orm/core";
 
 const app = new Elysia({ adapter: node() })
-	.get("/", () => "Hello Elysia")
+	.decorate("orm", orm)
+	.on("beforeHandle", () => RequestContext.enter(orm.em))
+	.on("afterHandle", ({ response }) =>
+		Utils.isEntity(response) ? wrap(response).toObject() : response,
+	)
 	.listen(3000, ({ hostname, port }) => {
 		console.log(`🦊 Elysia is running at ${hostname}:${port}`);
 	});
