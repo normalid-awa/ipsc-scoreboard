e1fc4f744ee428c87866eaaa1b43bec8acd6f7b0
jkh675 <rluo2018@gmail.com>
2025-09-01 22:14:47 +0800

    feat(api): Migrate entities

---
+++ apps/api/src/database/entities/member.entity.ts
@@ -0,0 +1,33 @@
+import {
+	Entity,
+	ManyToOne,
+	PrimaryKey,
+	Property,
+	type Rel,
+} from "@mikro-orm/core";
+import { Organization } from "./Organization.js";
+import { User } from "./User.js";
+
+@Entity()
+export class Member {
+	@PrimaryKey({ type: "text" })
+	id!: string;
+
+	@ManyToOne({
+		entity: () => Organization,
+		deleteRule: "cascade",
+	})
+	organization!: Rel<Organization>;
+
+	@ManyToOne({
+		entity: () => User,
+		deleteRule: "cascade",
+	})
+	user!: Rel<User>;
+
+	@Property({ type: "text" })
+	role!: string;
+
+	@Property({ fieldName: "createdAt", columnType: "timestamp(6)" })
+	createdAt!: Date;
+}
---
+++ apps/api/src/database/entities/session.entity.ts
@@ -0,0 +1,44 @@
+import {
+	Entity,
+	ManyToOne,
+	PrimaryKey,
+	Property,
+	type Rel,
+} from "@mikro-orm/core";
+import { User } from "./User.js";
+
+@Entity()
+export class Session {
+	@PrimaryKey({ type: "text" })
+	id!: string;
+
+	@Property({ fieldName: "expiresAt", columnType: "timestamp(6)" })
+	expiresAt!: Date;
+
+	@Property({ type: "text", unique: "session_token_key" })
+	token!: string;
+
+	@Property({ fieldName: "createdAt", columnType: "timestamp(6)" })
+	createdAt!: Date;
+
+	@Property({ fieldName: "updatedAt", columnType: "timestamp(6)" })
+	updatedAt!: Date;
+
+	@Property({ fieldName: "ipAddress", type: "text", nullable: true })
+	ipAddress?: string;
+
+	@Property({ fieldName: "userAgent", type: "text", nullable: true })
+	userAgent?: string;
+
+	@ManyToOne({
+		entity: () => User,
+		deleteRule: "cascade",
+	})
+	user!: Rel<User>;
+
+	@Property({
+		type: "text",
+		nullable: true,
+	})
+	activeOrganization?: string;
+}
