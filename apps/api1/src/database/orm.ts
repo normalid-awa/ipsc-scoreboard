d3118cac71dca4f15d8997cdec3acf625b45ee33
jkh675 <rluo2018@gmail.com>
2025-09-04 18:02:14 +0800

    style(api): Remove old api app

--- apps/api1/src/database/orm.ts
+++
@@ -1,6 +0,0 @@
-import { MikroORM } from "@mikro-orm/postgresql";
-import ormConfig from "./mikro-orm.config.js";
-
-const orm = await MikroORM.init(ormConfig);
-await orm.migrator.up();
-export default orm;
