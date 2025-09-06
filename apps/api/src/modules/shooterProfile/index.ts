5ec3dce5777a4830fd36d96c3dc1163ff947b90c
jkh675 <rluo2018@gmail.com>
2025-09-06 21:36:41 +0800

    feat(api): Implement update and delete on shooter profile

--- apps/api/src/modules/shooterProfile/index.ts
+++ apps/api/src/modules/shooterProfile/index.ts
@@ -9,6 +9,11 @@ import {
 } from "@/util/pagination.js";
 import { Elysia, status, t } from "elysia";
 
+const createShooterDto = t.Object({
+	identifier: t.String(),
+	sport: t.Enum(Sport),
+});
+
 export const shooterProfileRoute = new Elysia({
 	prefix: "/shooter-profile",
 })
@@ -53,9 +58,45 @@ export const shooterProfileRoute = new Elysia({
 		},
 		{
 			isAuth: true,
-			body: t.Object({
-				identifier: t.String(),
-				sport: t.Enum(Sport),
-			}),
+			body: createShooterDto,
+		},
+	)
+	.put(
+		"/:id",
+		async ({ orm, user, params, body }) => {
+			const shooterProfile = await orm.em.findOne(
+				ShooterProfile,
+				params.id,
+			);
+			if (!shooterProfile) return status(404);
+			if (shooterProfile.user !== orm.em.getReference(User, user.id))
+				return status(401);
+			shooterProfile.identifier = body.identifier;
+			shooterProfile.sport = body.sport;
+			orm.em.persist(shooterProfile).flush();
+			return shooterProfile;
+		},
+		{
+			isAuth: true,
+			params: t.Object({ id: t.Numeric({ minimum: 1 }) }),
+			body: createShooterDto,
+		},
+	)
+	.delete(
+		"/:id",
+		async ({ orm, user, params }) => {
+			const shooterProfile = await orm.em.findOne(
+				ShooterProfile,
+				params.id,
+			);
+			if (!shooterProfile) return status(404);
+			if (shooterProfile.user !== orm.em.getReference(User, user.id))
+				return status(401);
+			orm.em.remove(shooterProfile).flush();
+			return status(204);
+		},
+		{
+			isAuth: true,
+			params: t.Object({ id: t.Numeric({ minimum: 1 }) }),
 		},
 	);
