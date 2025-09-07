2b3c4007444031ec8ce7d2d7224d5462b0549ef0
jkh675 <rluo2018@gmail.com>
2025-09-07 16:20:58 +0800

    fix(web): Fix wrong domain in ssr

--- apps/web/src/api.ts
+++ apps/web/src/api.ts
@@ -2,6 +2,15 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "@ipsc_scoreboard/api";
 import env from "./env";
 
-const { api } = treaty<App>(env.VITE_BACKEND_API_URL);
+let apiUrl = "";
+
+if (globalThis.window) apiUrl = location.origin;
+else apiUrl = env.VITE_BACKEND_API_URL;
+
+const { api } = treaty<App>(apiUrl, {
+	headers: {
+		credentials: "include",
+	},
+});
 
 export { api };
