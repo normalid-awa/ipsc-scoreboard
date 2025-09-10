5b124a07ad67f63218d96fddc7140ba3c0b35858
jkh675 <rluo2018@gmail.com>
2025-09-10 22:08:09 +0800

    refactor(web): Add a tempory code of tanstack db

--- apps/web/src/queries/shooterProfile/shooterProfile.ts
+++ apps/web/src/queries/shooterProfile/shooterProfile.ts
@@ -1,8 +1,17 @@
 import { api } from "@/api";
 import { authClient } from "@/auth/auth.client";
 import { Sport } from "@ipsc_scoreboard/api";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { createCollection } from "@tanstack/db";
+import {
+	QueryClient,
+	useMutation,
+	useQuery,
+	useQueryClient,
+} from "@tanstack/react-query";
 import { useConfirm } from "material-ui-confirm";
+import { queryCollectionOptions } from "@tanstack/query-db-collection";
+import z from "zod";
+import { useLiveQuery } from "@tanstack/react-db";
 
 const shooterProfileQueryKey = ["shooterProfile"];
 
@@ -99,3 +108,63 @@ export function useDeleteShooterProfile() {
 		},
 	});
 }
+
+// const queryClient = new QueryClient();
+
+// const shooterProfileSchema = z.object({
+// 	id: z.number(),
+// 	sport: z.nativeEnum(Sport),
+// 	identifier: z.string(),
+// });
+
+// const shooterProfileCollection = createCollection(
+// 	queryCollectionOptions({
+// 		queryKey: ["shooterProfile"],
+// 		queryClient,
+// 		schema: shooterProfileSchema,
+// 		queryFn: async () =>
+// 			await api["shooter-profile"]
+// 				.get({ query: { first: 20 } })
+// 				.then((r) => r.data?.items || []),
+// 		getKey: (item) => item.id,
+// 		onInsert: async (item) => {
+// 			for (const mutation of item.transaction.mutations) {
+// 				api["shooter-profile"].post({
+// 					identifier: mutation.modified.identifier,
+// 					sport: mutation.modified.sport,
+// 				});
+// 			}
+// 		},
+// 		onUpdate: async (item) => {
+// 			for (const mutation of item.transaction.mutations) {
+// 				api["shooter-profile"]({ id: mutation.modified.id }).put({
+// 					identifier: mutation.modified.identifier,
+// 					sport: mutation.modified.sport,
+// 				});
+// 			}
+// 		},
+// 		onDelete: async (item) => {
+// 			for (const mutation of item.transaction.mutations) {
+// 				api["shooter-profile"]({ id: mutation.modified.id }).delete();
+// 			}
+// 		},
+// 	}),
+// );
+
+// export const useShooterProfiles = () =>
+// 	useLiveQuery((q) => q.from({ shooterProfiles: shooterProfileCollection }));
+
+// export const useMutateShooterProfile = (
+// 	shooterProfile: z.infer<typeof shooterProfileSchema>,
+// ) =>
+// 	shooterProfileCollection.update(shooterProfile.id, (d) => ({
+// 		...d,
+// 		...shooterProfile,
+// 	}));
+
+// export const useCreateShooterProfile = (
+// 	shooterProfile: z.infer<typeof shooterProfileSchema>,
+// ) => shooterProfileCollection.insert(shooterProfile);
+
+// export const useDeleteShooterProfile = (id: number) =>
+// 	shooterProfileCollection.delete(id);
