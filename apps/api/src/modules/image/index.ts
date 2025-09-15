9dbbdeaa1f87bf52ba12e0dd79c2a5f393bfa0a5
jkh675 <rluo2018@gmail.com>
2025-09-15 21:24:57 +0800

    feat(api): Add upload image api

--- apps/api/src/modules/image/index.ts
+++ apps/api/src/modules/image/index.ts
@@ -3,7 +3,12 @@ import orm from "@/database/orm.js";
 import { authPlugin } from "@/plugins/auth.js";
 import { envPlugin } from "@/plugins/env.js";
 import { Elysia, file, status, t } from "elysia";
+import { mkdir, rename, rm, stat } from "fs/promises";
+import crypto from "crypto";
 import path from "path";
+import { createWriteStream } from "fs";
+import { rel } from "@mikro-orm/core";
+import { User } from "@/database/entities/user.entity.js";
 
 export const imageRoute = new Elysia({
 	prefix: "/image",
@@ -33,6 +38,66 @@ export const imageRoute = new Elysia({
 		{
 			params: t.Object({
 				id: t.String({ format: "uuid" }),
+			}),
+		},
+	)
+	.post(
+		"/",
+		async ({ env, user, body }) => {
+			let tempFilePath = path.join(
+				env.FILE_UPLOAD_PATH,
+				`${Math.random().toString(36).substring(2, 15)}`,
+			);
+
+			try {
+				await stat(path.dirname(tempFilePath));
+			} catch (e) {
+				await mkdir(path.dirname(tempFilePath), { recursive: true });
+			}
+
+			try {
+				const hash = crypto.createHash("sha256");
+				const writeStream = createWriteStream(tempFilePath);
+				for await (const chunk of body.file.stream().values()) {
+					const data = Buffer.from(chunk);
+					writeStream.write(data);
+					hash.update(data);
+				}
+				writeStream.end();
+				writeStream.close();
+				const hashValue = hash.digest("hex");
+
+				const filePath = path.join(env.FILE_UPLOAD_PATH, hashValue);
+
+				try {
+					await stat(filePath);
+					console.log(`${hashValue} File already exists`);
+					await rm(tempFilePath);
+				} catch (e) {
+					await rename(tempFilePath, filePath);
+				}
+
+				const image = new Image();
+				image.filename = body.file.name;
+				image.mimetype = body.file.type;
+				image.size = body.file.size;
+				image.hash = hashValue;
+				image.uploader = rel(User, user.id);
+
+				await orm.em.persist(image).flush();
+				return image;
+			} catch (e) {
+				console.error(e);
+				await rm(tempFilePath);
+				return status(500);
+			}
+		},
+		{
+			isAuth: true,
+			body: t.Object({
+				file: t.File({
+					type: "image",
+				}),
 			}),
 		},
 	)
