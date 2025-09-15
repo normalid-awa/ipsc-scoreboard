3978371c1934ebb7ebe0c3a5698d04d3b815fcac
jkh675 <rluo2018@gmail.com>
2025-09-15 19:19:49 +0800

    feat(api): Add delete image api

--- apps/api/src/modules/image/index.ts
+++ apps/api/src/modules/image/index.ts
@@ -1,5 +1,6 @@
 import { Image } from "@/database/entities/image.entity.js";
 import orm from "@/database/orm.js";
+import { authPlugin } from "@/plugins/auth.js";
 import { envPlugin } from "@/plugins/env.js";
 import { Elysia, file, status, t } from "elysia";
 import path from "path";
@@ -8,10 +9,17 @@ export const imageRoute = new Elysia({
 	prefix: "/image",
 })
 	.use(envPlugin)
+	.use(authPlugin)
 	.get(
 		"/:id",
 		async ({ params, env, set }) => {
-			const res = await orm.em.findOne(Image, { uuid: params.id });
+			const res = await orm.em.findOne(
+				Image,
+				{ uuid: params.id },
+				{
+					cache: [`image:${params.id}`, 60e3 * 5 /* 5 minutes */],
+				},
+			);
 			if (!res) return status(404);
 
 			set.headers["pragma"] = "public";
@@ -23,6 +31,23 @@ export const imageRoute = new Elysia({
 			return file(path.join(env.FILE_UPLOAD_PATH, res.hash));
 		},
 		{
+			params: t.Object({
+				id: t.String({ format: "uuid" }),
+			}),
+		},
+	)
+	.delete(
+		"/:id",
+		async ({ user, params }) => {
+			const image = await orm.em.findOne(Image, { uuid: params.id });
+			if (!image) return status(404);
+			if (image.uploader.id !== user.id) return status(401);
+			await orm.em.remove(image).flush();
+			await orm.em.clearCache(`image:${params.id}`);
+			return status(204);
+		},
+		{
+			isAuth: true,
 			params: t.Object({
 				id: t.String({ format: "uuid" }),
 			}),
