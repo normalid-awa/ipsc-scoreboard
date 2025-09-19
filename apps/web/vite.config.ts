4225c467eab628c5cd4b3e7b6fa93bc4d9ece07d
jkh675 <rluo2018@gmail.com>
2025-09-19 23:10:56 +0800

    fix(web): User cf worker as build target

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -37,7 +37,7 @@ const config = defineConfig((confEnv) => {
 			tailwindcss(),
 			tanstackStart({
 				customViteReactPlugin: true,
-				target: "cloudflare-module",
+				target: "cloudflare-worker",
 			}),
 			viteReact(),
 			devtools({
