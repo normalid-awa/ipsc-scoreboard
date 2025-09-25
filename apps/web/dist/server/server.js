19742c31ca365d35502834fd7e1399dd01bd6162
jkh675 <rluo2018@gmail.com>
2025-09-25 22:46:00 +0800

    ci(web): Fix elysia server being exposed to client

--- apps/web/dist/server/server.js
+++ apps/web/dist/server/server.js
@@ -98,7 +98,7 @@ const VIRTUAL_MODULES = {
 async function loadVirtualModule(id) {
   switch (id) {
     case VIRTUAL_MODULES.startManifest:
-      return await import("./assets/_tanstack-start-manifest_v-89pxGXQh.js");
+      return await import("./assets/_tanstack-start-manifest_v-DHCBebUw.js");
     case VIRTUAL_MODULES.serverFnManifest:
       return await import("./assets/_tanstack-start-server-fn-manifest_v-DtgTK7xl.js");
     case VIRTUAL_MODULES.injectedHeadScripts:
@@ -442,7 +442,7 @@ function createStartHandler(cb) {
   let routerEntry = null;
   const getEntries = async () => {
     if (routerEntry === null) {
-      routerEntry = await import("./assets/router-Cqcdkyi6.js").then((n) => n.r);
+      routerEntry = await import("./assets/router-DWZZbF7A.js").then((n) => n.r);
     }
     if (startEntry === null) {
       startEntry = await import("./assets/start-B7Xlu2BJ.js");
