64f4520d7fcb5409977cd0f195d356038cb57981
jkh675 <rluo2018@gmail.com>
2025-09-26 23:25:20 +0800

    fix(web): Attempting to fix cloudflare worker env var issue

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,5 +1,4 @@
 import { createEnv } from "@t3-oss/env-core";
-import { vite } from "@t3-oss/env-core/presets-zod";
 import { z } from "zod";
 
 console.error(process.env.VITE_BACKEND_API_URL, process.env, import.meta.env);
