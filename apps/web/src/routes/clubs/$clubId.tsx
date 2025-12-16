5b2051a54c8a0ac379c1fb536e08b359b36d6ab2
jkh675 <rluo2018@gmail.com>
2025-12-16 18:43:33 +0800

    feat(web): Add tabs to club detail page

--- apps/web/src/routes/clubs/$clubId.tsx
+++ apps/web/src/routes/clubs/$clubId.tsx
@@ -6,15 +6,26 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
 import Typography from "@mui/material/Typography";
 import { createFileRoute, notFound } from "@tanstack/react-router";
+import { zodValidator } from "@tanstack/zod-adapter";
 import { useConfirm } from "material-ui-confirm";
+import z from "zod";
+
+const tabs = z.union([z.literal("members"), z.literal("statistics")]);
+
+const clubDetailsSearchScheme = z.object({
+	tab: z.optional(tabs).default("members"),
+});
 
 export const Route = createFileRoute("/clubs/$clubId")({
 	component: RouteComponent,
+	validateSearch: zodValidator(clubDetailsSearchScheme),
+	loaderDeps: ({ ...search }) => ({ ...search }),
 	async loader(ctx) {
 		const club = (
 			await ctx.context.api.club({ clubId: ctx.params.clubId }).get()
@@ -35,6 +46,8 @@ const ICON_SIZE = 120;
 
 function RouteComponent() {
 	const club = Route.useLoaderData();
+	const search = Route.useSearch();
+	const navigate = Route.useNavigate();
 	const confirm = useConfirm();
 
 	return (
@@ -50,7 +63,13 @@ function RouteComponent() {
 						borderRadius: (t) => t.shape.borderRadius,
 					}}
 				/>
-				<Paper sx={{ p: 2 }}>
+				<Paper
+					sx={{
+						p: 2,
+						position: "sticky",
+						top: 60,
+					}}
+				>
 					<Stack direction={"row"}>
 						<Box
 							component="img"
@@ -110,6 +129,30 @@ function RouteComponent() {
 				</Paper>
 				<Paper sx={{ height: 60 }}>
 					<FeaturePlaceHolder name={"Actions group"} />
+				</Paper>
+				<Paper>
+					<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+						<Tabs
+							value={search.tab}
+							onChange={(_, v) =>
+								navigate({
+									search: {
+										...search,
+										tab: v,
+									},
+								})
+							}
+							aria-label="basic tabs example"
+						>
+							{tabs._def.options.map((v) => (
+								<Tab
+									label={v.value}
+									key={v.value}
+									value={v.value}
+								/>
+							))}
+						</Tabs>
+					</Box>
 				</Paper>
 			</Stack>
 		</Container>
